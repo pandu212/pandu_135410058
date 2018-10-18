441713f4f28ba2ac681c4037547f13895bcca5d8
pandu212 <smokahontas@gmail.com>
2018-10-18 14:01:11 +0700

    coba

---
+++ webtcc/tes/gallery.php
@@ -0,0 +1,16 @@
+<?php include_once ('top_template.php'); ?>
+<div class="halaman">
+  <h3>Gallery</h3>
+</div>
+<div class="container">
+      <div class="col1">
+              <a href="">
+            <figure>
+              <img id="sfo1" src="images/vapor.jpg" alt="">
+            </figure>
+              <h1></h1>
+              <p></p>
+            </a>
+      </div> 
+    <?php include_once ('bottom_template.php');
+?>
---
+++ webtcc/tes/index.php
@@ -0,0 +1,19 @@
+<?php include_once ('template_atas.php'); ?>
+
+	<div class="intro">
+          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.jpg" alt="">
+          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Vapor</span>
+              <span class="section-heading-lower">Vapor atau Vape</span>
+            </h2>
+            <p class="mb-3">Vapor - apa itu vapor atau rokok elektrik. Vape atau yang lebih dikenal dengan Vapor adalah rokok elektrik yakni sebuah alternatif dari produk tembakau sebagai pengganti rokok. ... Yang dibahas adalah tentang nikotin yang digunakan di dalam vaping, tapi tidak semua liquid berbahan nikotin cair.
+            </p>
+            <div class="intro-button mx-auto">
+              <a class="btn btn-primary btn-xl" href="gallery.html">View Gallery!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php include_once ('template_bawah.php'); ?>
